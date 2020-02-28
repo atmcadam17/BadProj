7c572457f2c2f2dbf70656de811a73e22bf05c2b
George Machado <gm2564@nyu.edu>
2020-02-27 21:10:12 -0500

    added blinking

--- Assets/Scripts/State/BlobStateBlinking.cs
+++ Assets/Scripts/State/BlobStateBlinking.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class BlobStateBlinking : BlobStatePulsing
 {
+    private bool isBkinking = false;
+    private float blinkInterval;
+    private float blink;
+    private float totalBlinkTime;
+    private MeshRenderer blobMR;
+    
     public BlobStateBlinking(Blob theBlob) : base(theBlob)
     {
         
@@ -12,16 +18,53 @@ public class BlobStateBlinking : BlobStatePulsing
     public override void Enter()
     {
         base.Enter();
+        blinkInterval = .5f;
+        blink = 0f;
+        totalBlinkTime = 10f;
+        blobMR = blob.GetComponent<MeshRenderer>();
     }
 
     public override void Run()
     {
-        
+        totalBlinkTime -= Time.deltaTime;
+        if (totalBlinkTime > 0)
+        {
+            
+            Blink();
+            
+        }
+        else
+        {
+            blob.ChangeState(new BlobStateMoving(blob));
+        }
+
     }
 
+    public void Blink()
+    {
+
+        blink += Time.deltaTime;
+
+
+        if (blink > blinkInterval)
+        {
+            if (isBkinking) blobMR.enabled = false;
+            else
+            {
+                
+                blobMR.enabled = true;
+            }
+
+            isBkinking = !isBkinking;
+        }
+    }
+    
+    
     public override void Leave()
     {
         base.Leave();
+        GameController controller = blob.GetComponentInParent<GameController>();
+        controller.Score += 1;
     }
 }
 
